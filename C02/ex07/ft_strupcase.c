4a30669ef06b26954b56954e035206a02dd7037b
RolandVrignon <roland.vrignon@gmail.com>
2022-03-12 18:59:47 +0100

    ft_strupcase

---
+++ C02/ex07/ft_strupcase.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvrignon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/12 18:54:27 by rvrignon          #+#    #+#             */
+/*   Updated: 2022/03/12 18:54:31 by rvrignon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <unistd.h>
+
+char	ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ('a' <= str[i] && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (*str);
+}
+// int	main(void)
+// {
+//     char str[] = "TESTmarmeladeMARSEILLE";
+//     ft_strupcase(str);
+// }
