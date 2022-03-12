a7934c7dff8a03cfbfec8617bfb61e0af1152058
RolandVrignon <roland.vrignon@gmail.com>
2022-03-12 17:18:56 +0100

    add ft_str_is_numeric.c

--- C02/ex03/ft_str_is_numeric.c
+++ C02/ex03/ft_str_is_numeric.c
@@ -1,26 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rvrignon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/12 16:31:08 by rvrignon          #+#    #+#             */
-/*   Updated: 2022/03/12 16:46:32 by rvrignon         ###   ########.fr       */
+/*   Created: 2022/03/12 17:12:45 by rvrignon          #+#    #+#             */
+/*   Updated: 2022/03/12 17:18:07 by rvrignon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <unistd.h>
+// #include <unistd.h>
 
-int	ft_str_is_alpha(char *str)
+int	ft_str_is_numeric(char *str)
 {
 	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (!('A' <= str[i] && str[i] <= 'Z')
-			&& !('a' <= str[i] && str[i] <= 'z'))
+		if (!('0' <= str[i] && str[i] <= '9'))
 		{
 			return (0);
 		}
@@ -30,6 +29,6 @@ int	ft_str_is_alpha(char *str)
 }
 // int	main(void)
 // {
-// 	char str[] = "dasjkhdJ/KHKJH";
-// 	ft_str_is_alpha(str);
+// 	char str[] = "Test123";
+// 	ft_str_is_numeric(str);
 // }
