162b94992248905d0e21fb9b0129634578a196c2
RolandVrignon <roland.vrignon@gmail.com>
2022-03-13 15:11:51 +0100

    minor changes

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,56 +6,37 @@
 /*   By: rvrignon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 19:06:01 by rvrignon          #+#    #+#             */
-/*   Updated: 2022/03/13 03:40:28 by rvrignon         ###   ########.fr       */
+/*   Updated: 2022/03/13 15:08:37 by rvrignon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-char	gain_of_line(char *str, int i, int up, int low)
+char	ft_strcapitalize(char *str)
 {
+	int	i;
+
+	i = 0;
 	while (str[i] != '\0')
 	{
-		if (('a' <= str[i] && str[i] <= 'z') && up == 1)
-		{
+		if ('a' <= str[i] && str[i] <= 'z')
 			str[i] = str[i] - 32;
-			up = 0;
-			low = 1;
-		}
-		else if (('A' <= str[i] && str[i] <= 'Z') && low == 1)
-		{
-			str[i] = str[i] + 32;
-			low = 0;
-		}
-		else if (str[i] >= 48 && str[i] <= 57)
-			up = 0;
-		else if (str[i] <= 64 || (str[i] >= 91 && str[i] <= 96))
-		{
-			up = 1;
-			low = 0;
-		}
-		else if (up == 1)
-			up = 0;
+		i++;
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (('A' <= str[i] && str[i] <= 'Z')
+			&& (('a' <= str[i - 1] && str[i - 1] <= 'z')
+				|| ('A' <= str[i - 1] && str[i - 1] <= 'Z')
+				|| ('0' <= str[i - 1] && str[i - 1] <= '9')))
+				str[i] = str[i] + 32;
 		write(1, &str[i], 1);
 		i++;
 	}
 }
-
-char	ft_strcapitalize(char *str)
-{
-	int		i;
-	int		uppercase;
-	int		lowercase;
-
-	i = 0;
-	uppercase = 1;
-	lowercase = 0;
-	gain_of_line(str, i, uppercase, lowercase);
-	return (*str);
-}
-
 // int	main(void)
 // {
-//     char str[] = "salut, coMment tu vas ? 42mots cinquante+et+un";
+//     char str[] = "salut, coMMEnt TU /vas ? 42mots cinquante+et+un";
 //     ft_strcapitalize(str);
 // }
