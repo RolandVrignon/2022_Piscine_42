775d59bf6436a9928dade31291bf715f4da7e9f4
RolandVrignon <roland.vrignon@gmail.com>
2022-03-14 23:03:01 +0100

    putnbr_base

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -6,48 +6,45 @@
 /*   By: rvrignon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 21:23:09 by rvrignon          #+#    #+#             */
-/*   Updated: 2022/03/14 21:23:12 by rvrignon         ###   ########.fr       */
+/*   Updated: 2022/03/14 23:01:24 by rvrignon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
-int check_base(char *base)
+int	check_base(char *base)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    i = 0;
-    while (base[i] != '\0')
-    {
-        if (base[i] == '-' || base[i] == '+')
-            return (0);
-        j = 0;
-        while (j < i)
-        {
-            if (base[i] == base[j])
-                return(0);
-            j++;
-        }
-        i++;
-    }
-
-    if (i <= 1)
-        return (0);
-
-    return (i);    
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if (base[i] == '-' || base[i] == '+')
+			return (0);
+		j = 0;
+		while (j < i)
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	if (i <= 1)
+		return (0);
+	return (i);
 }
 
 void	ft_putnbr_base(int nbr, char *base)
 {
-	int i;
-    int long nb;
+	int		i;
+	long	nb;
 
-    i = check_base(base);
-    nb = nbr;
-    if ( i == 0 )
-        return;
+	i = check_base(base);
+	nb = nbr;
+	if (i == 0)
+		return ;
 	else if (nbr < 0)
 	{
 		write(1, "-", 1);
@@ -64,11 +61,9 @@ void	ft_putnbr_base(int nbr, char *base)
 		write(1, &base[nbr], 1);
 	}
 }
-
-int main(void)
-{
-    int nb = 12453;
-    char base[] = "01";
-    
-    ft_putnbr_base(nb, base);
-}
+// int main(void)
+// {
+//     int nb = 12453;
+//     char base[] = "01";   
+//     ft_putnbr_base(nb, base);
+// }
