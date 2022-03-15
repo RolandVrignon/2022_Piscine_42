34023d2a00ba7981c99f1fa48259e523cce31ae0
RolandVrignon <roland.vrignon@gmail.com>
2022-03-15 15:42:45 +0100

    minor changes

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -6,37 +6,34 @@
 /*   By: rvrignon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 15:08:02 by rvrignon          #+#    #+#             */
-/*   Updated: 2022/03/14 15:43:20 by rvrignon         ###   ########.fr       */
+/*   Updated: 2022/03/15 15:40:02 by rvrignon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include <stdio.h>
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
-{
-	unsigned int	i;
+{	
+	unsigned char	u1;
+	unsigned char	u2;	
 
-	i = 0;
-	while (s1[i] == s2[i] && i <= n)
+	while (n-- > 0)
 	{
-		if (s2[i] == '\0' && s1[i] != '\0')
-			return (100);
-		else if (s1[i] == '\0' && s2[i] != '\0' )
-			return (-100);
-		else if (s1[i] == '\0' && s2[i] == '\0')
+		u1 = (unsigned char) *s1++;
+		u2 = (unsigned char) *s2++;
+		if (u1 != u2)
+			return (u1 - u2);
+		if (u1 == '\0')
 			return (0);
-		i++;
 	}
-	if (s1[i] != s2[i])
-		return (s1[i] - s2[i]);
 	return (0);
 }
 
 // int main(void)
 // {
-//     char s1[] = "Hello";
-//     char s2[] = "Hella";
-//     int n = 6;
+//     char s1[] = "Hello Warld";
+//     char s2[] = "Hello World";
+//     int n = 10;
 //     int r;
 
 //     r = ft_strncmp(s1, s2, n); 
