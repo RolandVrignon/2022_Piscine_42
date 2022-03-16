f6949319bcbcf6e1aa20ecd296207a55b5a7be7d
RolandVrignon <roland.vrignon@gmail.com>
2022-03-16 13:13:57 +0100

    Start C05

---
+++ C05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvrignon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/16 12:55:30 by rvrignon          #+#    #+#             */
+/*   Updated: 2022/03/16 13:12:17 by rvrignon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	r;
+
+	r = 1;
+	if (!(nb >= 1))
+		return (0);
+	while (nb >= 1)
+	{
+		r = r * nb;
+		nb--;
+	}
+	return (r);
+}
+
+// int main()
+// {
+//     printf("Resultat :%d", ft_iterative_factorial(5));
+// }
---
+++ C05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvrignon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/16 12:55:30 by rvrignon          #+#    #+#             */
+/*   Updated: 2022/03/16 13:13:30 by rvrignon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	if (!(nb >= 1))
+		return (0);
+	else if (nb == 1)
+		return (1);
+	else
+		return (nb * ft_recursive_factorial(nb - 1));
+}
+
+//int main()
+//{
+//    printf("Resultat :%d", ft_recursive_factorial(4));
+//}
