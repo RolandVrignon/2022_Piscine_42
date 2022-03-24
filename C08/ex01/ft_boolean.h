4753432f09bac86a415bff8ee537712ae6e62bf7
Roland Vrignon <roland@Debian.home.com>
2022-03-24 15:31:09 +0100

    minor changes

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -19,8 +19,8 @@ typedef int		t_bool;
 # define FALSE 0
 # define TRUE 1
 # define SUCCESS 0
-# define EVEN_MSG "I have an even number of arguments."
-# define ODD_MSG "I have an odd number of arguments."
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
 # define EVEN(nbr) nbr % 2 == 0
 
 #endif
