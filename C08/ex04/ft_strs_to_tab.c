a740c1cc0649ff46bcc4dc91d561aa0d065c8303
RolandVrignon <roland.vrignon@gmail.com>
2022-03-29 15:39:58 +0200

    minor changes

--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -6,48 +6,60 @@
 /*   By: rvrignon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 16:31:43 by rvrignon          #+#    #+#             */
-/*   Updated: 2022/03/23 16:32:33 by rvrignon         ###   ########.fr       */
+/*   Updated: 2022/03/28 15:16:21 by rvrignon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdlib.h>
-#include <ft_stock_str.h>
-
-// typedef struct s_stock_str
-// {
-// 	int		size;
-// 	char	*str;
-// 	char	*copy;
-// }	t_stock_str;
+#include <stddef.h>
+#include "ft_stock_str.h"
 
 int	ft_strlen(char *str)
 {
-	int	i;
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*dest;
 
 	i = 0;
-	while (str[i] != '\0')
+	dest = malloc(sizeof(char) * (ft_strlen(src) + 1));
+	if (!(dest))
+		return (NULL);
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
 		i++;
-	return (i);
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
 struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
 {
-	int			i;
-	int			j;
-	t_stock_str	*tab;
+	int					i;
+	struct s_stock_str	*tab;
 
-	tab = malloc(sizeof(t_stock_str) * (ac + 1));
-	if (!tab)
-		return (0);
 	i = 0;
+	tab = malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!(tab))
+		return (NULL);
 	while (i < ac)
 	{
 		tab[i].size = ft_strlen(av[i]);
 		tab[i].str = av[i];
-		tab[i].copy = av[i];
+		tab[i].copy = ft_strdup(av[i]);
 		i++;
 	}
 	tab[i].str = 0;
+	tab[i].size = 0;
+	tab[i].copy = 0;
 	return (tab);
 }
--- C08/ex05/ft_show_tab.c
+++ C08/ex05/ft_show_tab.c
@@ -6,122 +6,75 @@
 /*   By: rvrignon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 16:19:29 by rvrignon          #+#    #+#             */
-/*   Updated: 2022/03/23 16:24:30 by rvrignon         ###   ########.fr       */
+/*   Updated: 2022/03/28 15:21:14 by rvrignon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <ft_stock_str.h>
-
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-typedef struct s_stock_str
-{
-	int		size;
-	char	*str;
-	char	*copy;
-}	t_stock_str;
-
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
-{
-	int			i;
-	int			j;
-	t_stock_str	*tab;
-
-	tab = malloc(sizeof(t_stock_str) * (ac + 1));
-	if (!tab)
-		return (0);
-	i = 0;
-	while (i < ac)
-	{
-		tab[i].size = ft_strlen(av[i]);
-		tab[i].str = av[i];
-		tab[i].copy = av[i];
-		i++;
-	}
-	tab[i].str = 0;
-	return (tab);
-}
+#include "ft_stock_str.h"
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void	ft_putnbr(int nb)
+void	ft_putstr(char *str)
 {
-	if (nb == -2147483648)
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
 	{
-		write(1, "-2147483648", 11);
-		return ;
-	}
-	if (nb < 0)
-	{
-		ft_putchar('-');
-		ft_putnbr(nb * -1);
-		return ;
-	}
-	if (nb >= 10)
-	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
-	}
-	else
-	{
-		ft_putchar(nb + '0');
+		ft_putchar(str[i]);
+		i++;
 	}
 }
 
-void	ft_putstr(char *str)
+void	ft_putnbr(int nb)
 {
-	int	n;
-
-	n = 0;
-	while (str[n] != '\0')
+	if (nb == -2147483648)
+		write(1, "-2147483648", 11);
+	else
 	{
-		write(1, &str[n], 1);
-		++n;
+		if (nb < 0)
+		{
+			ft_putchar('-');
+			nb -= nb;
+		}
+		if (nb >= 10)
+		{
+			ft_putnbr(nb / 10);
+			ft_putnbr(nb % 10);
+		}
+		else
+			ft_putchar(nb + 48);
 	}
-	write(1, "\n", 1);
 }
 
 void	ft_show_tab(struct s_stock_str *par)
 {
-	if (!par)
-		return ;
-	while (par->str)
+	int	i;
+
+	i = 0;
+	while (par[i].str)
 	{
-		ft_putstr(par->str);
-		write(1, "\n", 1);
-		ft_putnbr(par->size);
-		write(1, "\n", 1);
-		ft_putstr(par->copy);
-		write(1, "\n", 1);
-		par++;
+		ft_putstr(par[i].str);
+		ft_putchar('\n');
+		ft_putnbr(par[i].size);
+		ft_putchar('\n');
+		ft_putstr(par[i].copy);
+		ft_putchar('\n');
+		i++;
 	}
 }
+// int	main(int ac, char **av)
+// {
+// 	t_stock_str *tab;
 
+//     tab = ft_strs_to_tab(ac, av);
+// 	tab[0].str = "roland";
+//     if(tab)
+//         ft_show_tab(tab);
 
-int	main(int ac, char **av)
-{
-	t_stock_str *tab;
-
-    tab = ft_strs_to_tab(ac, av);
-	tab[0].str = "roland";
-    if(tab)
-        ft_show_tab(tab);
-
-    return (0);
-}
+//     return (0);
+// }
